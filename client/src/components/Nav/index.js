96646c79f4283de68cc0bd224b804a789a1e1885
z9265mx <u.meneses1993@gmail.com>
2019-04-20 10:50:17 -0400

    pulling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,55 +1,91 @@
-// import React from "react";
-// import "./style.css";
+// // import React from "react";
+// // import "./style.css";
 
-// function Nav() {
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-//       <a className="navbar-brand" href="/">
-//         React Reading List
-//       </a>
-//     </nav>
-//   );
+// // function Nav() {
+// //   return (
+// //     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+// //       <a className="navbar-brand" href="/">
+// //         React Reading List
+// //       </a>
+// //     </nav>
+// //   );
+// // }
+
+// // export default Nav;
+
+// import React from 'react';
+// import "./style.css";
+// import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+// export default class Example extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.toggleNavbar = this.toggleNavbar.bind(this);
+//     this.state = {
+//       collapsed: true
+//     };
+//   }
+
+//   toggleNavbar() {
+//     this.setState({
+//       collapsed: !this.state.collapsed
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <Navbar color="faded" light>
+//           <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
+//           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+//           <Collapse isOpen={!this.state.collapsed} navbar>
+//             <Nav navbar>
+//               <NavItem>
+//                 <NavLink href="/components/">Components</NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+//               </NavItem>
+//             </Nav>
+//           </Collapse>
+//         </Navbar>
+//       </div>
+//     );
+//   }
 // }
 
-// export default Nav;
 
 import React from 'react';
-import "./style.css";
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Media } from 'reactstrap';
+import homeIcon from '../images/house.jpg';
 
-export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
+const Example = () => {
+  return (
+    <Media>
+      <Media left href="#">
+        <Media object data-src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Gingerbread_House_Essex_CT.jpg/220px-Gingerbread_House_Essex_CT.jpg" alt="Generic placeholder image" />
+      </Media>
+      <Media body>
+        <Media heading>
+          Media heading
+        </Media>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+      </Media>
+    </Media>
+  );
+};
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+export default Example;
 
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-  render() {
-    return (
-      <div>
-        <Navbar color="faded" light>
-          <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse isOpen={!this.state.collapsed} navbar>
-            <Nav navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
-}
+
+var homeImg = document.getElementById('home');
+homeImg.src = homeIcon;
+
+console.log("Welcome! Greetings from app.js. Let's learn Webpack2");
+
+console.log(helperModule.greetings);
+
+var arr=[ 1, 2, 3];
+_.each(arr,function(val) {
+ console.log('Output from Lodash _.each for Element ' + val); 
+});
