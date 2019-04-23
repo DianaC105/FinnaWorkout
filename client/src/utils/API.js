fb13431b0bb377303b04b35b4eefe64851f3de7e
xpetrus <phsiao24@gmail.com>
2019-04-23 02:08:00 -0400

    login authentication and routing

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,32 @@
 import axios from "axios";
 
-export default{
+export default {
+  /*
+  loginCreds = {username: "peter", "password": password}
+  */
+ login: function(loginCreds){
+   return axios.post('/api/user/login', loginCreds)
+ },
+ //path to cehck if user is logged in
+ loginCheck: function(){
+   return axios.get('/api/user/login')
+ },
+
+ //path to log out
+ logout: function(){
+   return axios.get('/api/user/logout')
+ },
+ /*
+  Path to register new username + password must exist
+  e.g.
+  userInfo = {
+    username: "peter",
+    password: password
+  }
+  */
+ register: function(userInfo) {
+   return axios.post("/api/user/register", userInfo)
+ },
 
   getWorkouts: function(){
     return axios.get("/api/workouts");
@@ -8,4 +34,4 @@ export default{
   getMuscleGroup: function(musGrop){
     return axios.get("api/workouts/" + musGrop );
   }
-};
+}
