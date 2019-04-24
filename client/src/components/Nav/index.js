de6458b2882e9f4a7abea2dbdfd83c81d2f56d61
z9265mx <u.meneses1993@gmail.com>
2019-04-23 20:09:05 -0400

    Pulling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -56,41 +56,6 @@
 
 
 import React from 'react';
-<<<<<<< HEAD
-import { Media } from 'reactstrap';
-import homeIcon from '../images/house.jpg';
-
-const Example = () => {
-  return (
-    <Media>
-      <Media left href="#">
-        <Media object data-src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Gingerbread_House_Essex_CT.jpg/220px-Gingerbread_House_Essex_CT.jpg" alt="Generic placeholder image" />
-      </Media>
-      <Media body>
-        <Media heading>
-          Media heading
-        </Media>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-      </Media>
-    </Media>
-  );
-};
-
-export default Example;
-
-
-var homeImg = document.getElementById('home');
-homeImg.src = homeIcon;
-
-console.log("Welcome! Greetings from app.js. Let's learn Webpack2");
-
-console.log(helperModule.greetings);
-
-var arr=[ 1, 2, 3];
-_.each(arr,function(val) {
- console.log('Output from Lodash _.each for Element ' + val); 
-});
-=======
 import "./style.css";
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
@@ -131,4 +96,3 @@ class Example extends React.Component {
   }
 }
 export default Example;
->>>>>>> ad856fd1ec1446a6e0ac43da799a621b7f0c86d4
