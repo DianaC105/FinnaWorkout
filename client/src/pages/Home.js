db4237790218601d39bdd928d3e5d8a3e1cd8136
unknown <mrtoryan@outlook.com>
2019-04-23 20:57:40 -0400

    data passed via props to options

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,54 +8,94 @@ import Form from "../components/Form"
 import Workoutpick from "../components/WorkoutCard";
 export default class Home extends Component{
 
-state = {
-  
-      
+state = {   
       workouts: [],
       plan: [],
       message: "Putting togeather you're plan now",
       q:"",
       chestWorkouts: [],
       backWorkouts: [],
-      legsWorkouts: [],
       absWorkouts: [],
-      muscleGroups: []
-    };
+      muscleGroups: [],
+      chestOptionNames: [],
+      backOptionNames: [],
+      absOptionNames: [],
+      mapObjects: ["chest", "back", "abs"]
+};
 
     
     filterWorkouts(){
       let chestArray = this.filterBodyParts(this.state.workouts, "chest");
-      let legsArray = this.filterBodyParts(this.state.workouts, "legs");
       let backArray = this.filterBodyParts(this.state.workouts, "back");
       let absArray = this.filterBodyParts(this.state.workouts, "abs");
       this.setState({
         chestWorkouts: chestArray,
         backWorkouts: backArray,
-        legsWorkouts: legsArray,
-        absWorkouts: absArray
+        absWorkouts: absArray,
+      }, () => {
+        console.log(this.state);
+        console.log(this.state.chestWorkouts);
+        this.updateWorkoutsByBodyPart(this.state.chestWorkouts);
+        this.updateWorkoutsByBodyPart(this.state.backWorkouts);
+        this.updateWorkoutsByBodyPart(this.state.absWorkouts);
       })
-      let muscleGroups = []
-      muscleGroups.push(chestArray,backArray,legsArray,absArray)
-      console.log(muscleGroups)
+        
+        let muscleGroupsArr = []
+
+      let {chestWorkouts, backWorkouts, absWorkouts} = this.state;
+      muscleGroupsArr.push(chestWorkouts);
+      muscleGroupsArr.push(backWorkouts);
+      muscleGroupsArr.push(absWorkouts);
+
       this.setState({
-        muscleGroups: muscleGroups
-
+        muscleGroups: muscleGroupsArr
       })
-
       
+    }
+    
+    updateWorkoutsByBodyPart = (objects) => {
+      let chestWorkouts = [];
+      let backWorkouts = [];
+      let absWorkouts = [];
+      
+      objects.forEach(object => {
+        if (object.bodyPart === "chest") {
+          chestWorkouts.push(object.name);
+          this.setState({
+            chestOptionNames: chestWorkouts
+          }, () => {console.log(this.state.chestOptionNames); })
+        }
+        else if (object.bodyPart === "back") {
+          backWorkouts.push(object.name);
+          this.setState({
+            backOptionNames: backWorkouts
+          }, () => {console.log(this.state.backOptionNames); })
+        }
+        else if (object.bodyPart === "abs"){
+          absWorkouts.push(object.name);
+          this.setState({
+            absOptionNames: absWorkouts
+          },
+           () => {console.log(this.state.absOptionNames); })
+        }
+      })
+      console.log(chestWorkouts);
+      console.log(backWorkouts)
+      console.log(absWorkouts)
+    }
+    
+    componentWillMount(){
+      console.log("mounting")
+      this.getWorkouts() 
+      // this.updateWorkoutsByBodyPart(this.state.chestWorkouts);
     }
 
     componentDidMount(){
-      console.log("mounted")
-      this.getWorkouts()
-      
-      
-      
+      // this.updateWorkoutsByBodyPart(this.state.chestWorkouts);
     }
     
     filterBodyParts(array,bodyPart){
       return array.filter(workout => workout.bodyPart === bodyPart )
-      
     };
     
     getWorkouts = () => {
@@ -79,46 +119,32 @@ state = {
       );
     };
     
+    checkBodyPart= (object) => {
+      if (object === "chest") {
+        return this.state.chestOptionNames;
+      } else if (object === "back") {
+        return this.state.backOptionNames;
+      } else if (object === "abs") {
+        return this.state.absOptionNames;
+      }
+    }
+
     render(){
-      console.log(this.state)
+      
+    
+
     return (
       <div>
         <Example />
         <Wrapper>
         <Jumbotron /> FinnaWorkOUT
-        <Roulette
-        />
-        {this.state.muscleGroups.map(muscleGroups=>(
+        {this.state.mapObjects.map(object => (
 
-        <Workoutpick
-          id={muscleGroups.id}
-          key={muscleGroups.id}
-          name={muscleGroups.name}
-          
-        />
-        ))}
-         
-        
-
-
-        {/* {this.state.workoutpick.map(workoutpick => (
-       
-        <Workoutpick
-            id={workoutpick.id}
-            key={workoutpick.id}
-            name={workoutpick.name}
-            image={workoutpick.image}
-            occupation={workoutpick.occupation}
-            location={workoutpick.location}
-          />
-        ))} */}
-        {/* <Roulette 
-          chestWorkouts={this.state.chestWorkouts} 
-          backWorkouts={this.state.backWorkouts}
-          legsWorkouts={this.state.legsWorkouts}
-          absWorkouts={this.state.absWorkouts}
-        /> */}
-        <Form />
+          <Workoutpick
+              options={this.checkBodyPart(object)}
+            />
+            ))}
+          <Form />
         </Wrapper>
       </div>
     )
